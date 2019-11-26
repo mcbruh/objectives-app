3da7637f5eb0755de5d06191821ed961da8a2116
Michael Chen <michael.chen.1120@gmail.com>
2019-11-25 21:49:01 -0500

    Add tests for users#show action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -30,4 +30,13 @@ RSpec.describe UsersController, type: :controller do
             end
         end
     end
+
+    describe 'GET #show' do
+        it 'renders the user show page' do
+            user = User.create!(username:'testuser', password:'good_password')
+            get :show, params: { id: user.id }
+            expect(response).to render_template('show')
+            expect(response).to have_http_status(200)
+        end
+    end
 end

37ded5dc5844185f21a1419f6ea065e6d9b1dc7c
Michael Chen <michael.chen.1120@gmail.com>
2019-11-29 01:56:32 -0500

    Refactor goal crud spec with helper method. Add goal show view. Add new goal partial and page

--- spec/features/goal_crud_spec.rb
+++ spec/features/goal_crud_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 feature 'CRUD actions for goals' do
-    given(:new_user) { create(:new_user) }
+    given(:crud_test) { create(:new_user) }
 
     background(:each) do
-      log_in_as(new_user)
+        log_in_as(crud_test)
     end
 
     feature 'creating a goal' do
-    
-      scenario 'should render new goal page' do
-        visit new_goal_url
-        expect(page).to have_content 'new goal!'
-      end
-    
-      scenario 'should show goal after creating' do
-        make_goal('testing goals', 'I made a goal work!')
-        expect(page).to have_content, 'I made a goal work!'
-      end
+        scenario 'it should render new goal page' do
+          visit new_goal_url
+          expect(page).to have_content 'new goal!'
+        end
+      
+        scenario 'it should show goal after creating' do
+          make_goal('testing goals', 'I made a goal work!')
+          expect(page).to redirect_to user_url(crud_test.id)
+          expect(page).to have_content, 'I made a goal work!'
+        end
       
     end
 

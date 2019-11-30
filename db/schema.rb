f70e0648a87f00730d6bad1324e06e5e1526905d
Michael Chen <michael.chen.1120@gmail.com>
2019-11-30 15:05:15 -0500

    Migrate user comments and goal comments. Add comments_spec features test.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_033111) do
+ActiveRecord::Schema.define(version: 2019_11_30_032115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goal_comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id", null: false
+    t.integer "goal_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["goal_id", "user_id"], name: "index_goal_comments_on_goal_id_and_user_id", unique: true
+  end
 
   create_table "goals", force: :cascade do |t|
     t.string "title", null: false
@@ -25,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_11_27_033111) do
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_goals_on_title"
     t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
+  create_table "user_comments", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_comments_on_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
